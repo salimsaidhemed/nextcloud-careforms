ea96fffadbae1bdbb0ca4498887473effff14fe8
salimsaidhemed <35762985+salimsaidhemed@users.noreply.github.com>
2026-09-10 16:56:04 +0200

    Repair form version column for existing dev installs

---
+++ lib/Migration/Version000209Date20260910170000.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OCA\CareForms\Migration;
+
+use Closure;
+use OCP\DB\ISchemaWrapper;
+use OCP\Migration\IOutput;
+use OCP\Migration\SimpleMigrationStep;
+
+class Version000209Date20260910170000 extends SimpleMigrationStep
+{
+    public function changeSchema(IOutput $output, Closure $schemaClosure, array $options): ?ISchemaWrapper
+    {
+        /** @var ISchemaWrapper $schema */
+        $schema = $schemaClosure();
+        if (!$schema->hasTable('careforms_form_versions')) {
+            return $schema;
+        }
+
+        $table = $schema->getTable('careforms_form_versions');
+        if ($table->hasColumn('version') && !$table->hasColumn('version_number')) {
+            $table->renameColumn('version', 'version_number');
+        }
+
+        return $schema;
+    }
+}
