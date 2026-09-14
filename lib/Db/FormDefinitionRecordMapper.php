745b128b3935eede5f08e8173bd9cc9924f91a6e
salimsaidhemed <35762985+salimsaidhemed@users.noreply.github.com>
2026-09-14 23:43:20 +0200

    Add compatible persisted definition lookup

--- lib/Db/FormDefinitionRecordMapper.php
+++ lib/Db/FormDefinitionRecordMapper.php
@@ -44,6 +44,23 @@ class FormDefinitionRecordMapper extends QBMapper
         }
     }
 
+    public function findLatestAtOrBefore(string $formId, int $version): ?FormDefinitionRecord
+    {
+        $qb = $this->db->getQueryBuilder();
+        $qb->select('*')
+            ->from($this->getTableName())
+            ->where($qb->expr()->eq('form_id', $qb->createNamedParameter($formId)))
+            ->andWhere($qb->expr()->lte('form_version', $qb->createNamedParameter($version)))
+            ->orderBy('form_version', 'DESC')
+            ->setMaxResults(1);
+
+        try {
+            return $this->findEntity($qb);
+        } catch (DoesNotExistException | MultipleObjectsReturnedException) {
+            return null;
+        }
+    }
+
     public function findByFormAndVersion(string $formId, int $version): ?FormDefinitionRecord
     {
         $qb = $this->db->getQueryBuilder();
