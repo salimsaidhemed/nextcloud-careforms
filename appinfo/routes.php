a64fee766956b9256646e580248ad458002f9157
salimsaidhemed <35762985+salimsaidhemed@users.noreply.github.com>
2026-09-14 10:57:33 +0200

    Add JSON import export routes

--- appinfo/routes.php
+++ appinfo/routes.php
@@ -8,6 +8,8 @@ return [
         ['name' => 'report#overview', 'url' => '/api/reports/overview', 'verb' => 'GET'],
         ['name' => 'report#submissions', 'url' => '/api/reports/submissions', 'verb' => 'GET'],
         ['name' => 'formAdmin#index', 'url' => '/api/forms/admin', 'verb' => 'GET'],
+        ['name' => 'formAdmin#export', 'url' => '/api/forms/admin/{formId}/export', 'verb' => 'GET'],
+        ['name' => 'formAdmin#validateImport', 'url' => '/api/forms/admin/import/validate', 'verb' => 'POST'],
         ['name' => 'formAdmin#update', 'url' => '/api/forms/admin/{formId}', 'verb' => 'PUT'],
         ['name' => 'formAdmin#createDraft', 'url' => '/api/forms/admin/{formId}/versions', 'verb' => 'POST'],
         ['name' => 'formAdmin#publish', 'url' => '/api/forms/admin/{formId}/versions/{version}/publish', 'verb' => 'POST'],
